0329999387f4b0f08291b234e1232d428ac0c086
hubhe <lapidusy@gmail.com>
2024-03-18 19:14:02 +0200

    remove likes and replys

--- src/models/comment_model.ts
+++ src/models/comment_model.ts
@@ -4,8 +4,6 @@ export interface IComment{
   item_id: string,
   user_id: string,
   comment: string,
-  replays: Array<string>,
-  likes: number,
 }
 
 const commentSchema = new mongoose.Schema<IComment>({
@@ -20,13 +18,7 @@ const commentSchema = new mongoose.Schema<IComment>({
   comment: {
     type: String,
     required: true
-  },
-  replays: {
-    type: [String]
-  },
-  likes: {
-    type: Number
-  },
+  }
 });
 
 export default mongoose.model<IComment>("Comment", commentSchema);

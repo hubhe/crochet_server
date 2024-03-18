cbe8610a5653b497b42eb0c4d19217a05e15a82e
hubhe <lapidusy@gmail.com>
2024-03-18 20:31:57 +0200

    addded uploader

--- src/models/item_model.ts
+++ src/models/item_model.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export interface Iitem {
   _id: string;
   name: string;
+  uploader: string;
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   picture: string;
   description: string;
@@ -12,6 +13,9 @@ const itemSchema = new mongoose.Schema({
     name: {
       type: String,
       required: true,
+    },
+    uploader: {
+      type: String
     },
     commnets: {
       type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }]

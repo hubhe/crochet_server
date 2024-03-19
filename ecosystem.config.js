81ceee7d8204064e4c8a18c4e9fb11fbf3ed0d00
aditall <70978533+aditall@users.noreply.github.com>
2024-03-19 21:28:24 +0200

    tsnode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    apps : [{
-      name: 'my-app',
-      script: './src/server.ts', // Entry point of your application
-      watch: true, // Enable auto-restart on file changes
-      env: {
-        NODE_ENV: 'development' // Environment variables for development
-      },
-      env_production: {
-        NODE_ENV: 'production' // Environment variables for production
-      }
-    }]
-  };
-  
+  apps : [{
+    name: 'my-app',
+    script: './src/server.ts', // Entry point of your application
+    watch: true, // Enable auto-restart on file changes
+    interpreter: '/usr/bin/ts-node', // Absolute path to ts-node
+    env: {
+      NODE_ENV: 'development' // Environment variables for development
+    },
+    env_production: {
+      NODE_ENV: 'production' // Environment variables for production
+    }
+  }]
+};
